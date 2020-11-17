f0aaa689a4a5de8debc6c15fd32673141aa5e1e6
Andrew Perry <andrew.perry@rseg.com>
2020-11-17 08:38:52 -0500

    Add anagram test

---
+++ anagram.py
@@ -0,0 +1,19 @@
+def anagram(word_left: str, word_right: str) -> bool:
+    """
+    An anagram is a word or phrase formed by rearranging the letters of a
+    different word or phrase, typically using all the original letters exactly
+    once. For example, the word `anagram` can be rearranged into `nag a ram`
+    - wikipedia : https://en.wikipedia.org/wiki/Anagram
+    This function should ignore white space and capitalization for the check
+    Parameters
+    ----------
+    word_left : str
+    The first word for anagram comparison
+    word_right : str
+    The second word for anagram comparison
+    Returns
+    -------
+    bool
+    True iff the words are anagrams
+    """
+    pass
